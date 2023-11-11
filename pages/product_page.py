7f60cb6a635feb2f8d2826219c673daf013bfa36
Aleksandr Simonov <sav8817@mail.ru>
2023-11-11 15:48:15 +0300

    refactoring

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,3 +6,5 @@ class ProductPage(BasePage):
     def add_to_cart_button(self):
         basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         basket.click()
+    def message_product_to_cart(self):
+        assert self.is_element_present(*ProductPageLocators.)
