e6339d66f727e4e0ea86d5c7b652f4eb26de9670
Aleksandr Simonov <sav8817@mail.ru>
2023-11-11 12:46:47 +0300

    refactoring

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,6 @@ class LoginPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
