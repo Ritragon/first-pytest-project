7a6b21994049d5a66427daf8807072f61c6763d8
Aleksandr Simonov <sav8817@mail.ru>
2023-11-13 19:53:13 +0300

    paramettrize xfail

--- pages/test_product_page.py
+++ pages/test_product_page.py
@@ -1,9 +1,12 @@
 from pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('promo_offer', [pytest.param(num, marks=pytest.mark.xfail)for num in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_btn()
