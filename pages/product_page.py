e4013a963b3b726f306eb97d4d47cf07809f1af7
Aleksandr Simonov <sav8817@mail.ru>
2023-12-02 18:31:18 +0300

    code review

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,27 +4,32 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
 
-    def should_be_add_to_basket_btn(self):
-        assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), "Add to basket button isn't presented"
-
     def add_to_cart_button(self):
         basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         basket.click()
 
-    def message_product_to_name(self):
-        name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        basket_name = self.browser.find_element(*ProductPageLocators.BASKET_NAME).text
-        assert name == basket_name, "Product name not found on message"
+    def should_be_add_to_basket_btn(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), \
+            "Add to basket button isn't presented"
 
-    def message_product_to_price(self):
-        price = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_NAME).text
-        assert price == basket_price, "Product price not found on message"
+    def should_be_success_message(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should be"
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
 
+    def success_message_product_to_name(self):
+        name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        basket_name = self.browser.find_element(*ProductPageLocators.BASKET_NAME).text
+        assert name == basket_name, "Product name not found on message"
+
     def success_message_should_disappear(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but it must disappear"
+
+    def total_should_be_equal_to_price(self):
+        price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        info = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+        assert price in info, "total price in the alert doesn't match the product price"
