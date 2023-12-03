b1af20eb57149537b7ea37a520bbd116cd56ebd0
Aleksandr Simonov <sav8817@mail.ru>
2023-12-03 14:39:45 +0300

    code for review

--- conftest.py
+++ conftest.py
@@ -9,25 +9,32 @@ def pytest_addoption(parser):
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="en",
                      help="Choose language")
+    parser.addoption('--headless', action='store_true', help="Run browser in headless mode")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
-
+    headless = request.config.getoption("headless")
     if browser_name == "chrome":
         options = Options()
+        if headless:
+            options.add_argument("--headless")
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options)
     elif browser_name == "firefox":
         options_firefox = OptionsFirefox()
+        if headless:
+            options_firefox.add_argument("--headless")
         options_firefox.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(options_firefox)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     browser.quit()
+
+
 @pytest.fixture(scope="session")
 def user_language(request):
     return request.config.getoption("language")
